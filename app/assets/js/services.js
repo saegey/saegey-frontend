d5feb31e629c2327f83847ccc84842fac99b6d8f
Adam Saegebarth <adams@synapse.com>
2014-06-29 13:44:27 -0700

    ngResource

--- app/assets/js/services.js
+++ app/assets/js/services.js
@@ -4,30 +4,30 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('app.services', ['ngResource'])
-  .factory('BikeDetail', function($resource) {
+  .factory('BikeDetail',  ['$resource', function($resource) {
     return $resource('http://api.saegey.com/v1/bikes/:tag', {
    	  tag: '@tag'
     })
-  })
-  .factory('Photos', function($resource) {
+  }])
+  .factory('Photos',  ['$resource', function($resource) {
     return $resource('http://api.saegey.com/v1/instagram/:type?limit=:limit', {
    	  tag: '@type',
    	  limit: '@limit',
     })
-  })
-  .factory('ActivityPlaces', function($resource) {
+  }])
+  .factory('ActivityPlaces',  ['$resource', function($resource) {
     return $resource('http://api.saegey.com/v1/moves/places?unique=true&page=:page&limit=:limit', {
    	  tag: '@page',
    	  limit: '@limit',
     })
-  })
-  .factory('ActivitySummary', function($resource) {
+  }])
+  .factory('ActivitySummary',  ['$resource', function($resource) {
     return $resource('http://api.saegey.com/v1/moves/summary?unique=true&page=:page&limit=:limit', {
    	  tag: '@page',
    	  limit: '@limit',
     })
-  })
-  .factory('Resume', function($resource) {
+  }])
+  .factory('Resume',  ['$resource', function($resource) {
   	return $resource('https://api.github.com/gists/1dc7a850a74e434507b3', {})
-  })
+  }])
   .value('version', '0.1');
