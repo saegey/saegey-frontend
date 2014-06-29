d2c576b0d8e355760e41edbf74ac5df7c6be5bc2
Adam Saegebarth <adams@synapse.com>
2014-06-29 12:27:24 -0700

    custom-build

--- Gulpfile.js
+++ Gulpfile.js
@@ -179,5 +179,15 @@ gulp.task('build', function () {
         });
 });
 
+// gulp.task('heroku:production', function () {
+
+//     runSequence(
+//         'clean',
+//         'usemin',
+//         function () {
+//             $.util.log('Build complete');
+//         });
+// });
+
 gulp.task('default', ['connect', 'watch']);
 gulp.task('serve', ['connect', 'watch']);
--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -65,20 +65,4 @@ app.controller('ResumeController', ['$scope', 'Resume', function ($scope, Resume
 }]);
 
 
-app.controller('ModalInstanceCtrl', ['$scope', function ($scope, $modalInstance, items) {
-
-  $scope.items = items;
-  $scope.selected = {
-    item: $scope.items[0]
-  };
-
-  $scope.ok = function () {
-    $modalInstance.close($scope.selected.item);
-  };
-
-  $scope.cancel = function () {
-    $modalInstance.dismiss('cancel');
-  };
-}]);
-
 
